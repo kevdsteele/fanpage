73bb57b70f0ffcad1293c26a114e89be305f562f
kevdsteele <48927209+kevdsteele@users.noreply.github.com>
2019-05-01 12:10:52 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 // Add your code here
-function openLink() {
+function openWebLink() {
   window.open("https://the1975.com/northamerica/");
+}
+
+function openFavLink() {
+  location.href = '#Bio';
+}
+
+function openMoreLink() {
+ location.href = 'http://your.url.here';
+}
+
+function openBioLink() {
+  location.href = 'http://your.url.here';
 }
